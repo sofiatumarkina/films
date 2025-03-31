1014b1c667ae28c72316d4178644119798868a1e
Sofia Tumarkina <sofyatumarkina31@gmail.com>
2025-03-31 21:00:38 +0300

    7

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -8,7 +8,10 @@ export function Movies(props) {
     return (
       <div className="movies">
         {movies.map((movie) => (
-          <Link to={`films/movie/${movie.imdbID}`} key={movie.imdbID}>
+          <Link
+            to={`/films/movie/${movie.imdbID}`}
+            key={movie.imdbID}
+          >
             <Movie {...movie} />
           </Link>
         ))}
